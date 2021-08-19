7d085de0e56654a69697b0fe0d71d07e81099fd3
a5892731 <a5892731@o2.pl>
2021-08-19 17:55:25 +0200

    new

--- listbox.py
+++ listbox.py
@@ -9,14 +9,27 @@
 
 from tkinter import *
 
-
+def lb_command():
+    x = listbox.get(listbox.curselection())
+    print(x)
 
 window = Tk() # instantiate an instance of a window
 window.geometry("420x420")
 
+listbox = Listbox(window, font = ("Times New Roman", 20))
+listbox.pack()
 
+listbox.insert(1, "pizza")
+listbox.insert(2, "pasta")
+listbox.insert(3, "hotdog")
+listbox.insert(4, "hamburger")
+listbox.insert(5, "soup")
+listbox.insert(6, "crapy chicken")
+listbox.config(height = listbox.size())
 
-
+submit_button = Button(window,  text = "submit", font = ("Times New Roman", 20),
+                       command = lb_command)
+submit_button.pack()
 
 #-------------------------------------------------------------
 window.mainloop() # place window on computer screen. listen for evants
