36d844ad4004f813c32555b15d37fa9c52f3187a
a5892731 <a5892731@o2.pl>
2021-08-19 15:39:46 +0200

    new

--- main.py
+++ main.py
@@ -9,36 +9,12 @@
 
 from tkinter import *
 
-count = 0
-def click():
-    global count
-    count += 1
-    print(count)
-
 
 
 window = Tk() # instantiate an instance of a window
 window.geometry("420x420")
 
-button = Button(window, text = "click me",
-                font= ('Arial', 40, 'bold'),
-                relief=RAISED,
-                bd=5,
-                command = click)
 
-button_text = "click me 2"
-button2 = Button(window, text = button_text,
-                font= ('Arial', 40, 'bold'),
-                relief=RAISED,
-                bd=5,
-                command = click,
-                fg = "green",
-                bg = "grey",
-                activeforeground = "red",
-                activebackground= "black",)
-
-button.pack()
-button2.pack()
 
 
 
